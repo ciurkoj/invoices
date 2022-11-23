59ef92f7307ce29aeea4e5872f65ff472a93093e
ciurkoj <jakub.ciurko@managemy.com>
2022-11-23 15:29:16 +0100

    added InvoiceCardWidget()

--- lib/pages/invoices_page.dart
+++ lib/pages/invoices_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:invoices/db/invoice_database.dart';
 import 'package:invoices/models/invoice.dart';
 import 'package:invoices/pages/add_invoice_form_page.dart';
+import 'package:invoices/widgets/invoice_card_widget.dart';
 
 class InvoicesPage extends StatefulWidget {
   const InvoicesPage({super.key});
@@ -114,36 +115,13 @@ class InvoicesPageState extends State<InvoicesPage> {
   Widget buildInvoices() => ListView.builder(
         itemCount: invoices.length,
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent), borderRadius: const BorderRadius.all(Radius.circular(8))),
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SvgPicture.asset(
-                        "assets/pdf-svgrepo-com.svg",
-                        height: 40,
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Invoice Id:\t\t\t\t${invoices[index].invoiceId!}", style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                        Text("Business Partner:\t\t\t\t${invoices[index].businessPartner}", style: const TextStyle(color: Colors.black54, fontSize: 20)),
-                        Text("Net Amount:\t\t\t\t${invoices[index].netAmount.toString()}", style: const TextStyle(color: Colors.black54, fontSize: 20)),
-                        Text("Gross Amount:\t\t\t\t${invoices[index].grossAmount}", style: const TextStyle(color: Colors.black54, fontSize: 20)),
-                        Text("VAT:\t\t\t\t${invoices[index].vat.toString()}%", style: const TextStyle(color: Colors.black54, fontSize: 20)),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+          return InvoiceCardWidget(
+              invoiceId: invoices[index].invoiceId!,
+              businessPartner: invoices[index].businessPartner,
+              netAmount: invoices[index].netAmount.toString(),
+              grossAmount: invoices[index].grossAmount,
+              vat: invoices[index].vat.toString(),
+              svgPath: "assets/pdf-svgrepo-com.svg");
         },
       );
 }
---
+++ lib/widgets/invoice_card_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class InvoiceCardWidget extends StatelessWidget {
+  String invoiceId;
+  String businessPartner;
+  String netAmount;
+  String grossAmount;
+  String vat;
+  String svgPath;
+
+  InvoiceCardWidget({Key? key,
+  required this.invoiceId,
+  required this.businessPartner,
+  required this.netAmount,
+  required this.grossAmount,
+  required this.vat,
+  required this.svgPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent), borderRadius: const BorderRadius.all(Radius.circular(8))),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  svgPath,
+                  height: 40,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Invoice Id:\t\t\t\t $invoiceId", style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  Text("Business Partner:\t\t\t\t$businessPartner", style: const TextStyle(color: Colors.black54, fontSize: 20)),
+                  Text("Net Amount:\t\t\t\t$netAmount", style: const TextStyle(color: Colors.black54, fontSize: 20)),
+                  Text("Gross Amount:\t\t\t\t$grossAmount", style: const TextStyle(color: Colors.black54, fontSize: 20)),
+                  Text("VAT:\t\t\t\t$vat", style: const TextStyle(color: Colors.black54, fontSize: 20)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );;
+  }
+}
